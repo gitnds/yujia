590fdf795ca74f308ee6254796227055510b82ec
gitnds <31470999+gitnds@users.noreply.github.com>
2017-09-04 15:13:19 +0800

    2

--- app/src/main/java/com/strong/yujiaapp/activity/AddInfoActivity.java
+++ app/src/main/java/com/strong/yujiaapp/activity/AddInfoActivity.java
@@ -1,6 +1,7 @@
 package com.strong.yujiaapp.activity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -9,7 +10,7 @@ import com.strong.yujiaapp.base.BaseActivity;
 
 public class AddInfoActivity extends BaseActivity {
 
-    private LinearLayout ll_return;
+    private LinearLayout ll_return, ll_loc,ll_hold;
     private TextView tv_title;
 
     @Override
@@ -21,18 +22,41 @@ public class AddInfoActivity extends BaseActivity {
         initView();
         initData();
         initEvent();
-        //56666666666666666666666666
 
     }
-    public void initView(){
+
+    public void initView() {
         ll_return = (LinearLayout) findViewById(R.id.ll_return);
+        ll_loc = (LinearLayout) findViewById(R.id.ll_loc);
+        ll_hold = (LinearLayout) findViewById(R.id.ll_hold);
         tv_title = (TextView) findViewById(R.id.tv_title);
     }
-    public void initData(){
+
+    public void initData() {
+
         tv_title.setText("新增地址");
     }
+
     public void initEvent() {
 
         ll_return.setOnClickListener(returnClickListener);
+        //选择省市区
+        ll_loc.setOnClickListener(locClickListener);
+        //保存
+        ll_hold.setOnClickListener(holdClickListener);
     }
+
+    View.OnClickListener locClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+        }
+    };
+
+    View.OnClickListener holdClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+        }
+    };
 }
