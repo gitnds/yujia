866ad2c649731e511f1b29918c78bc5c00fe5fdf
haiyun <123>
2017-09-04 14:08:23 +0800

    测试

---
+++ app/src/main/java/com/strong/yujiaapp/utils/OnItemClieckLinster.java
@@ -0,0 +1,9 @@
+package com.strong.yujiaapp.utils;
+import android.view.View;
+
+public interface OnItemClieckLinster{
+
+
+    void onItemClickListener(View view , int pos, String name);
+    void onItemLongClickListener(View view , int pos);
+}
