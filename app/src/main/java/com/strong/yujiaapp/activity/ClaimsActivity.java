e60f09d1e5a3220fe2e9da1fa36bc9a0cd36358b
gitnds <31470999+gitnds@users.noreply.github.com>
2017-09-06 10:17:42 +0800

    2

--- app/src/main/java/com/strong/yujiaapp/activity/ClaimsActivity.java
+++ app/src/main/java/com/strong/yujiaapp/activity/ClaimsActivity.java
@@ -1,20 +1,45 @@
 package com.strong.yujiaapp.activity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.strong.yujiaapp.R;
 import com.strong.yujiaapp.base.BaseActivity;
+import com.strong.yujiaapp.controls.DialogAg;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Administrator on 2017/8/22.
  */
 
-public class ClaimsActivity extends BaseActivity {
-    private LinearLayout ll_return;
+public class ClaimsActivity extends BaseActivity implements View.OnClickListener{
+    List<String> list ;
+    String[] goodsName = {"药品","服装","电脑","厨具","工具","配件","电器","家具","电脑","乐器","日用品","食品","书","行李","其他"};
+    String[] accident = {"丢失","火灾","交通肇事","破损","其他"};
+    @Override
+    public void onClick(View view) {
+        if (view.getId()==R.id.goods_name_linear){
+            list = new ArrayList<String>();
+            for (int i=0;i<goodsName.length;i++) {
+                list.add(goodsName[i]);
+            }
+            DialogAg.getListDialog(ClaimsActivity.this,list,metrics,tv_goods_name);
+        }else if (view.getId()==R.id.accident_linear){
+            list  = new ArrayList<String>();
+            for (int i=0;i<accident.length;i++) {
+                list.add(accident[i]);
+            }
+            DialogAg.getListDialog(ClaimsActivity.this,list,metrics,tv_accident);
+        }
+    }
 
-    private TextView tv_title;
+    private LinearLayout ll_return,accident_linear,goods_name_linear;
+
+    private TextView tv_title,tv_goods_name,tv_accident;
 
 
     @Override
@@ -31,8 +56,12 @@ public class ClaimsActivity extends BaseActivity {
     public void initView(){
 
         ll_return = (LinearLayout) findViewById(R.id.ll_return);
+        goods_name_linear = (LinearLayout) findViewById(R.id.goods_name_linear);
+        accident_linear = (LinearLayout) findViewById(R.id.accident_linear);
 
         tv_title = (TextView) findViewById(R.id.tv_title);
+        tv_goods_name = (TextView) findViewById(R.id.tv_goods_name);
+        tv_accident = (TextView) findViewById(R.id.tv_accident);
 
 
     }
@@ -43,6 +72,8 @@ public class ClaimsActivity extends BaseActivity {
     public void initEvent(){
 
         ll_return.setOnClickListener(returnClickListener);
+        goods_name_linear.setOnClickListener(this);
+        accident_linear.setOnClickListener(this);
 
     }
 
