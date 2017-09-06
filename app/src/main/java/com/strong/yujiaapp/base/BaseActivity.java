3be454a265350e310628188824a288474aea892f
gitnds <31470999+gitnds@users.noreply.github.com>
2017-09-06 10:18:03 +0800

    2

--- app/src/main/java/com/strong/yujiaapp/base/BaseActivity.java
+++ app/src/main/java/com/strong/yujiaapp/base/BaseActivity.java
@@ -2,17 +2,20 @@ package com.strong.yujiaapp.base;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
 import android.view.View;
 
 import com.strong.yujiaapp.R;
 
 public class BaseActivity extends AppCompatActivity {
-
+    public DisplayMetrics metrics;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_load);
         getSupportActionBar().hide();
+        metrics = new DisplayMetrics();
+        this.getWindowManager().getDefaultDisplay().getMetrics(metrics);
     }
 
     /**
